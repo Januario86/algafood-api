c83150613f29412b8c32187ee9f405cab9053d7e
Januario <januario86fp@gmail.com>
2023-04-20 15:26:00 -0300

    feat:add-lombok

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,12 +1,16 @@
 package com.algaworks.algafood.domain.model;
 
-import java.util.Objects;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@EqualsAndHashCode
 @Entity
 public class Cozinha {
 
@@ -14,34 +18,5 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY	)
 	private Long id;
 	
-	private String nome;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, nome);
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cozinha other = (Cozinha) obj;
-		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome);
-	}
+	private String nome;	
 }
