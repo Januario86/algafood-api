fa8a100a9befe685f3aecaffde2327e22cffbeb6
Januario <januario86fp@gmail.com>
2023-04-22 18:25:56 -0300

    fi:add-@Component

--- src/main/java/com/algaworks/algafood/infrastructure/repository/EstadoRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/EstadoRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.infrastructure.repository;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Estado;
@@ -10,6 +11,7 @@ import com.algaworks.algafood.domain.repository.EstadoRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
+@Component
 public class EstadoRepositoryImpl implements EstadoRepository {
 
     @PersistenceContext
