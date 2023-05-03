003635bcecb61a385bc5d88314c44bde9426a8cb
Januario <januario86fp@gmail.com>
2023-05-03 18:48:18 -0300

    feat:add-controller-estado

--- src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -3,22 +3,45 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Estado;
-import com.algaworks.algafood.domain.repository.EstadoRepository;
+import com.algaworks.algafood.domain.service.CadastroEstadoService;
 
 @RestController
 @RequestMapping("/estados")
 public class EstadoController {
 
 	@Autowired
-	private EstadoRepository estadoRepository;
+	private CadastroEstadoService cadastroEstadoService;
 	
 	@GetMapping
 	public List<Estado> listar(){
-		return estadoRepository.listar();
+		return cadastroEstadoService.listar();
+	}
+	
+	public Estado salvar(Estado estado) {
+		return cadastroEstadoService.salvar(estado);
+	}
+	
+	public void excluir(Long estadoId) {
+		try {
+			cadastroEstadoService.excluir(estadoId);
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(
+				String.format("Não existe um cadastro de estado com código %d", estadoId));
+		
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(
+				String.format("Estado de código %d não pode ser removido, pois está em uso", estadoId));
+		}
 	}
 }
